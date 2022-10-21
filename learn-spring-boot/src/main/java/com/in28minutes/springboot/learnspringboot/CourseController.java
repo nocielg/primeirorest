f14c430ccb26e8daf5a00aaf04adf464384433c4
nocielg <gleissonfreitas@hotmail.com>
2022-10-21 12:59:09 -0300

    Primeiro commit

---
+++ learn-spring-boot/src/main/java/com/in28minutes/springboot/learnspringboot/CourseController.java
@@ -0,0 +1,18 @@
+package com.in28minutes.springboot.learnspringboot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+	@RequestMapping("/courses")
+	public List<Course> retriveAllCourses(){
+		return Arrays.asList(
+				new Course (1, "Learn AWS", "in28minutes"),
+				new Course (2, "LearnDevOps", "in28minutes")
+		);
+	}
+}
